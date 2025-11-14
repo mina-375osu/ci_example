6eec0b579c5e46b42f5448a5e9352b6824159a95
Minako Koike <koikemi@oregonstate.edu>
2025-11-14 15:05:42 +0900

    Add unit tests for my_func

--- tests.py
+++ tests.py
@@ -1,4 +1,16 @@
 import unittest
+import task
+
+
+class TestCase(unittest.TestCase):
+
+    def test1(self):
+        expected = "Hello World"
+        self.assertEqual(task.my_func(), expected)
+
+    def test2(self):
+        expected = "Hola World"
+        self.assertEqual(task.my_func(), expected)
 
 if __name__ == '__main__':
     unittest.main()
